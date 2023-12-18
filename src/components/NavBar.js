3900d6929c42a1621db5a1ff21d2bfb13ff96622
Nivedita <nivedita.pagar@outlook.com>
2023-12-18 23:38:47 +0100

    Refactor components into separate files for better organization

---
+++ src/components/NavBar.js
@@ -0,0 +1,32 @@
+export default function NavBar({ children }) {
+  return <nav className="nav-bar">{children}</nav>;
+}
+
+export function Logo() {
+  return (
+    <div className="logo">
+      <span role="img">🎦</span>
+      <h1>Watchopia</h1>
+    </div>
+  );
+}
+
+export function Search({ query, setQuery }) {
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+  );
+}
+
+export function NumResults({ movies }) {
+  return (
+    <p className="num-results">
+      Found <strong>{movies.length}</strong> results
+    </p>
+  );
+}
