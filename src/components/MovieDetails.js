33e65689b929c02b6bcf96c1427c55ef50b9e17e
Nivedita <nivedita.pagar@outlook.com>
2023-12-19 21:03:34 +0100

    Add event listener for Escape key press to unselect movie

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -49,6 +49,22 @@ export default function MovieDetails({
 
   useEffect(
     function () {
+      function callback(e) {
+        if (e.code === "Escape") {
+          onCloseMovie();
+        }
+      }
+      document.addEventListener("keydown", callback);
+
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [onCloseMovie]
+  );
+
+  useEffect(
+    function () {
       async function getMovieDetails() {
         try {
           setIsLoading(true);
